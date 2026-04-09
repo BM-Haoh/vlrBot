9a524d6930429c5a6868224c01679173a4b55a60
BM-Haoh <joao.massabki@gmail.com>
2026-04-09 17:28:12 -0300

    BOTAPP v2.0 auto-scrapping no empty files

---
+++ src/auto_scrappet.py
@@ -0,0 +1,19 @@
+from auto import vlr_stealer
+from DB_handler import DB_handler
+
+def run():
+    try:
+        mtc_grt = vlr_stealer()
+        matches = mtc_grt.process_camps()
+        
+        if matches:
+            DBH = DB_handler(matches)
+            DBH.process_matches()
+        else:
+            print("Nenhuma partida nova para processar.")
+    except Exception as e:
+        print(f"Erro na execução: {e}")
+        exit(1) # Força o GitHub Actions a marcar como falha se der erro
+
+if __name__ == "__main__":
+    run()
