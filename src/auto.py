40516bbcd51e69dbe86a494955c2151f0596bc49
BM-Haoh <joao.massabki@gmail.com>
2026-04-10 12:44:41 -0300

    BOTAPP v2.0 main.py working without 'legacy' files

--- src/auto.py
+++ src/auto.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from tqdm import tqdm
-import new_api_handler as nah
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
